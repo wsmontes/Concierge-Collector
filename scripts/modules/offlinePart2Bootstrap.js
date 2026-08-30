a623e7d043c878d2d4c849dff40d996069ad26b7
Wagner Montes <wmontes@gmail.com>
2026-08-30 15:55:10 -0700

    feat: wire explicit discard media cleanup

--- scripts/modules/offlinePart2Bootstrap.js
+++ scripts/modules/offlinePart2Bootstrap.js
@@ -20,7 +20,8 @@
         ['OfflineCaptureProcessor', 'scripts/services/offlineCaptureProcessor.js?v=20260830-1'],
         ['OfflinePhotoProcessor', 'scripts/services/offlinePhotoProcessor.js?v=20260830-1'],
         ['OfflineSourceIdentityBridge', 'scripts/modules/offlineSourceIdentityBridge.js?v=20260830-1'],
-        ['OfflineKnownLinkageGuard', 'scripts/modules/offlineKnownLinkageGuard.js?v=20260830-1']
+        ['OfflineKnownLinkageGuard', 'scripts/modules/offlineKnownLinkageGuard.js?v=20260830-1'],
+        ['OfflineExplicitDiscardGuard', 'scripts/modules/offlineExplicitDiscardGuard.js?v=20260830-1']
     ];
 
     function loadScript(globalName, src) {
