7b43ebf5bfd1c438a64ccbc73a610da8a26e5a1d
Wagner Montes <wmontes@gmail.com>
2026-08-30 19:38:08 -0700

    feat: load curation authoring controller after offline policies

--- scripts/modules/offlinePart2Bootstrap.js
+++ scripts/modules/offlinePart2Bootstrap.js
@@ -18,6 +18,7 @@
         ['CurationOwnershipPolicy', 'scripts/services/curationOwnershipPolicy.js?v=20260830-1'],
         ['OfflineLinkingModule', 'scripts/modules/offlineLinkingModule.js?v=20260830-1'],
         ['OfflineOwnershipModule', 'scripts/modules/offlineOwnershipModule.js?v=20260830-1'],
+        ['CurationAuthoringController', 'scripts/modules/curationAuthoringController.js?v=20260830-1'],
         ['OfflineCaptureProcessor', 'scripts/services/offlineCaptureProcessor.js?v=20260830-1'],
         ['OfflinePhotoProcessor', 'scripts/services/offlinePhotoProcessor.js?v=20260830-1'],
         ['OfflineSourceIdentityBridge', 'scripts/modules/offlineSourceIdentityBridge.js?v=20260830-1'],
